4980e02c17b986091ec5c803d3d6feabb833ab8c
Daniel Moura <daniel-moura@outlook.com>
2022-12-30 12:33:55 -0300

    feat: implement route items on app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from routes.items import itemsRoute
+
 app = Flask(__name__)
 
 @app.route('/')
 def health_check():
   return 'Application is running!'
+
+app.register_blueprint(itemsRoute)
 
 if __name__ == '__main__':
   app.run()
